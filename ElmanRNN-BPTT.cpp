7e5476e7976ba968961b7608663c176189a499a5
Rohitash Chandra <c.rohitash@gmail.com>
2017-01-20 18:14:27 +1200

    Update ElmanRNN-BPTT.cpp

--- ElmanRNN-BPTT.cpp
+++ ElmanRNN-BPTT.cpp
@@ -1,6 +1,11 @@
 /*
  Dr. Rohitash Chandra.
  Coded in 2005. Updated 2016.
+ Based in: Werbos, Paul J. "Backpropagation through time: what it does and how to do it." Proceedings of the IEEE 78.10 (1990): 1550-1560.
+ 
+ https://en.wikipedia.org/wiki/Backpropagation_through_time
+ 
+ used in: https://github.com/rohitash-chandra/CooperativeCoevolution-RNN
  */
 #include <iostream>
 #include <stdlib.h>
